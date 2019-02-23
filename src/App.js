239a5d6aa9ea905f6188f62212444d7e2bd15951
tecnomage <tecnocratay@gmail.com>
2019-02-23 16:29:18 -0300

    inicio da configuracao do router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import {Provider} from './context' 
-import Contacts from './Components/contacts/Contacts';
-import AddContact from './Components/contacts/AddContact'
+import { Provider } from "./context";
+import Contacts from "./Components/contacts/Contacts";
+import AddContact from "./Components/contacts/AddContact";
 import Header from "./layout/Header";
+import About from "./pages/about";
 
 class App extends Component {
   render() {
     return (
       <Provider>
-      <div className="App">
-        <Header branding="Contact Manager" />
-        <div className="container">
-          <AddContact></AddContact>
-          <Contacts />
-        </div>
-      </div>
-      </Provider>
+        <Router>
+          <div className="App">
+            <Header branding="Contact Manager" />
+            <div className="container">
+              <Switch>
+                <Route exact path="/about" component={About} />
 
+                <Route exact path="/contact/add" component={AddContact} />
+
+                <Route exact path="/" component={Contacts} />
+              </Switch>
+            </div>
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
