a36dd768cab32f17624873e2dd8e836bd56a83ce
tecnomage <tecnocratay@gmail.com>
2019-02-26 15:05:52 -0300

    add delecao

--- src/Components/contacts/AddContact.js
+++ src/Components/contacts/AddContact.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import { Consumer } from "../../context";
-import uuid from 'uuid'
-import TextInputGroup from '../../layout/TextInputGroup'
+//import uuid from "uuid";
+import TextInputGroup from "../../layout/TextInputGroup";
+import Axios from "axios";
 
 class AddContact extends Component {
   state = {
@@ -12,60 +13,58 @@ class AddContact extends Component {
   };
 
   onChange = e => {
-    console.log(e.target)
+    console.log(e.target);
     this.setState({ [e.target.name]: e.target.value });
   };
 
   //name: 'name is required', email: 'email is required',
   //phone: 'phone is required'
-  onSubmit = (dispatch,e) => {
-     e.preventDefault();
-      //console.log(dispatch);
-      console.log(e.target.name)
-     const {name,email,phone,errors} = this.state;
-    
+  onSubmit = (dispatch, e) => {
+    e.preventDefault();
+    console.log(e.target.name);
+    const { name, email, phone, errors } = this.state;
+
     //validations
-    if(name===''){
-      this.setState( {errors: {name:'name is required'}});
+    if (name === "") {
+      this.setState({ errors: { name: "name is required" } });
       return;
     }
 
-    if(email===''){
-      this.setState( {errors: {email:'email is required'}});
-         return;
-    }
-
-    
-    if(phone===''){
-      this.setState({errors:'email is required'});
+    if (email === "") {
+      this.setState({ errors: { email: "email is required" } });
       return;
     }
-    
-     const newContact = {
-       id:uuid(),
-       name,
-       email,
-       phone,
-       errors: {}
 
-     }
+    if (phone === "") {
+      this.setState({ errors: "email is required" });
+      return;
+    }
 
-    dispatch({
-      type: "ADD_CONTACT",
-      payload: newContact
-    })
-  
+    const newContact = {
+      name,
+      email,
+      phone,
+      errors: {}
+    };
+
+    Axios.post("https://jsonplaceholder.typicode.com/users", newContact).then(
+      res =>
+        dispatch({
+          type: "ADD_CONTACT",
+          payload: res.data
+        })
+    );
+
     this.setState({
-      name: '',
-      email: '',
-      phone: '',
+      name: "",
+      email: "",
+      phone: "",
       error: {}
-    })
-    
-  this.props.history.push('/');
+    });
 
-    console.log(this.state)
-  
+    this.props.history.push("/");
+
+    console.log(this.state);
   };
 
   // static defaultProps = {
@@ -81,26 +80,34 @@ class AddContact extends Component {
       <Consumer>
         {value => {
           const { dispatch } = value;
-          
+
           return (
             <div>
               <div className="card-header">Add Contact</div>
               <div className="card-body">
-                <form onSubmit={ (e) => this.onSubmit(dispatch,e)}>
-                  <TextInputGroup  name="name" label="Name"
-                  placeholder='entre seu nome aqui'  value={name}
-                  onChange={this.onChange}
-                  error={errors.name}
-                  />
-                  <TextInputGroup type='email' name="email" label="Email"
-                  placeholder='entre seu email'  value={email}
-                  onChange={this.onChange}
-                  error={errors.email}
-                  />
-              
+                <form onSubmit={e => this.onSubmit(dispatch, e)}>
                   <div className="form-group">
-                    <label htmlFor="phone">Phone</label>
-                    <input
+                    <TextInputGroup
+                      name="name"
+                      label="Name"
+                      placeholder="entre seu nome aqui"
+                      value={name}
+                      onChange={this.onChange}
+                      error={errors.name}
+                    />
+                    <TextInputGroup
+                      type="email"
+                      name="email"
+                      label="Email"
+                      placeholder="entre seu email"
+                      value={email}
+                      onChange={this.onChange}
+                      error={errors.email}
+                    />
+
+                    <TextInputGroup
+                      label="Phone"
+                      name="phone"
                       type="number"
                       className="form-control form-control-lg"
                       placeholder="Enter telefone"
--- src/Components/contacts/Contacts.js
+++ src/Components/contacts/Contacts.js
@@ -23,7 +23,7 @@ class Contacts extends Component {
                 contact => (
                   <Contact key={contact.id} contact={contact} />
                 )
-                //console.log(contact)
+                
               )}
             </React.Fragment>
           );
