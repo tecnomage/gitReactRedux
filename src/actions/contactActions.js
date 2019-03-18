5e050d8e45e412c52c9286bb50fd78fac24970ef
tecnomage <tecnocratay@gmail.com>
2019-03-18 16:42:00 -0300

    insercao do delete Contact

--- src/actions/contactActions.js
+++ src/actions/contactActions.js
@@ -1,9 +1,27 @@
-import { GET_CONTACTS } from "./types";
+import { GET_CONTACTS, ADD_CONTACT,DELETE_CONTACT } from "./types";
 
 
 export const getContacts = () => {
     return {
         type: GET_CONTACTS
+    }
+
+};
+
+
+export const deleteContact = (id) => {
+    return {
+        type: DELETE_CONTACT,
+        payload : id
+    }
+
+};
+
+
+export const addContacts = (contact) => {
+    return {
+        type: ADD_CONTACT,
+        payload: contact
     }
 
 };
